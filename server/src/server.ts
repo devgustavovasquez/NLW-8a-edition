2aa984b994e9cc606d883088c19f670724b39450
Tafuso <gustavovasquez2002@gmail.com>
2022-05-10 08:02:12 -0300

    add port const

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import { routes } from './routes'
 
+const port = process.env.PORT || 3333
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(port, () => {
   console.log('HTTP server running!')
 })
